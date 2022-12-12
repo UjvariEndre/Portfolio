8d667d04ff1e4e9813a2299b6396a54fa4ad1bc8
UjvariEndre <ujvendre@gmail.com>
2022-12-12 13:14:17 +0100

    Adding reCAPTCHA, isomorphic-fetch node package

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv").config();
 const nodemailer = require("nodemailer");
+const fetch = require("isomorphic-fetch");
 
 const app = express();
 app.use(express.static("public"));
@@ -40,7 +41,28 @@ app.post("/", function(req, res) {
   const name = req.body.name;
   const email = req.body.email;
   const message = req.body.message;
-  mailWrapper(name, email, message).catch(console.error);
+  const url = `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.SECRET_KEY}&response=${req.body["g-recaptcha-response"]}`;
+  fetch(url, {
+    method: "post"
+  }).then((response) => response.json()).then((google_response) => {
+    if (google_response.success) {
+      if (google_response.score >= 0.5) {
+        mailWrapper(name, email, message).catch(console.error);
+        // popup: email has been sent
+        console.log("email has been sent");
+      } else {
+        // popup: "Warning, you have been banned for suspicious behavior, please try again later."
+        console.log("Warning, you have been banned for suspicious behavior, please try again later.");
+      }
+    } else {
+      //  popup: "Error with Google reCAPTCHA response"
+      console.log("Error with Google reCAPTCHA response");
+    }
+  }).catch((error) => {
+    return res.json({
+      error
+    });
+  });
   res.redirect("/");
 });
 
